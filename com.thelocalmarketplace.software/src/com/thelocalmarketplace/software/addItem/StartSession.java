524b270339608823f6adbeda957ba8c154455acc
Jack Graver <jackhgraver@gmail.com>
2023-11-16 10:19:44 -1000

    Hardware Project and Iteration 1
    Updated Hardware project code
    Added code from iteration 1

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/addItem/StartSession.java
@@ -0,0 +1,43 @@
+package com.thelocalmarketplace.software.addItem;
+
+import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+import powerutility.PowerGrid;
+
+public class StartSession {
+
+	
+	
+    private SelfCheckoutStation selfCheckoutStation;
+    // Precondition: System is not in a session
+    private boolean inSession = false;
+
+    
+    
+    public StartSession(SelfCheckoutStation station) {
+        selfCheckoutStation = station;
+    }
+
+    
+    /**
+     * Starts session
+     * @param powergrid 
+     * @return void
+     */
+    public void startSession(PowerGrid powerGrid) { 	
+        selfCheckoutStation.plugIn(powerGrid); 	// Plug in the devices to the power grid
+
+        
+        selfCheckoutStation.turnOn();		// Turn on the devices
+
+        inSession = true;
+    
+ 	
+        
+    }
+
+    
+    
+    public boolean isInSession() {
+        return inSession;
+    }
+}
