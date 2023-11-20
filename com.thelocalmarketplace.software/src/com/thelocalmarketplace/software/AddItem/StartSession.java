5ab9482971c9fcbe05a736976eeb0b62e4c7e41c
Jack Graver <46544214+JackGraver@users.noreply.github.com>
2023-11-20 16:13:52 -0700

    Update StartSession.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AddItem/StartSession.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AddItem/StartSession.java
@@ -1,42 +1,53 @@
-package com.thelocalmarketplace.software.addItem.AddItem;
+package com.thelocalmarketplace.software;
 
+import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationGold;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationSilver;
+
 import powerutility.PowerGrid;
 
 public class StartSession {
 
-	
-	
-    private SelfCheckoutStationGold selfCheckoutStation;
+    public SelfCheckoutStationGold selfCheckoutStationGold;
+    public SelfCheckoutStationSilver selfCheckoutStationSilver;
+    public SelfCheckoutStationBronze selfCheckoutStationBronze;
+
     // Precondition: System is not in a session
     private boolean inSession = false;
 
-    
-    
     public StartSession(SelfCheckoutStationGold station) {
-        selfCheckoutStation = station;
+        this.selfCheckoutStationGold = station;
     }
 
-    
+    public StartSession(SelfCheckoutStationSilver station) {
+        this.selfCheckoutStationSilver = station;
+    }
+
+    public StartSession(SelfCheckoutStationBronze station) {
+        this.selfCheckoutStationBronze = station;
+    }
+
     /**
      * Starts session
-     * @param powergrid 
+     *
+     * @param powerGrid
      * @return void
      */
-    public void startSession(PowerGrid powerGrid) { 	
-        selfCheckoutStation.plugIn(powerGrid); 	// Plug in the devices to the power grid
-
-        
-        selfCheckoutStation.turnOn();		// Turn on the devices
+    public void startSession(PowerGrid powerGrid) {
+        if (this.selfCheckoutStationGold != null) {
+            this.selfCheckoutStationGold.plugIn(powerGrid);
+            selfCheckoutStationGold.turnOn();
+        } else if (selfCheckoutStationSilver != null) {
+            this.selfCheckoutStationSilver.plugIn(powerGrid);
+            selfCheckoutStationSilver.turnOn();
+        } else if (selfCheckoutStationBronze != null) {
+            this.selfCheckoutStationBronze.plugIn(powerGrid);
+            selfCheckoutStationBronze.turnOn();
+        }
 
         inSession = true;
-    
- 	
-        
     }
 
-    
-    
     public boolean isInSession() {
         return inSession;
     }
