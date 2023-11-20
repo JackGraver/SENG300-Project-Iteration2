2ae99303f2f61bddce7b78794c01ba4176be0645
Jack Graver <jackhgraver@gmail.com>
2023-11-19 23:06:32 -0700

    Add Item via handheld + other iteration 1 files

---
+++ com.thelocalmarketplace.software/src/AddItemViaHandheld.java
@@ -0,0 +1,69 @@
+package com.thelocalmarketplace.software;
+
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.scanner.Barcode;
+import com.jjjwelectronics.scanner.BarcodeScannerListener;
+import com.jjjwelectronics.scanner.BarcodedItem;
+import com.jjjwelectronics.scanner.IBarcodeScanner;
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationGold;
+import com.thelocalmarketplace.hardware.external.ProductDatabases;
+import com.thelocalmarketplace.software.addItem.AddItemController;
+
+/*
+ * Jack Graver - 10187274
+ * .
+ * .
+ * .
+ */
+public class AddItemViaHandheld implements BarcodeScannerListener {
+
+    @Override
+    public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+    }
+
+    @Override
+    public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+    }
+
+    @Override
+    public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+    }
+
+    @Override
+    public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+    }
+
+    @Override
+    public void aBarcodeHasBeenScanned(IBarcodeScanner barcodeScanner, Barcode barcode) {
+
+        //not allow scan if customer session in progress already
+            //Need to check if StartSession inSession variable is true, either needs to be static or some type of implementation like singleton
+
+        //following steps from use case:
+        //Block?
+        AddItemController.block();
+
+        BarcodedProduct product = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
+        if(product != null) {
+            //determine characterisitcs (weight and cost) of product associated with barcode
+            double weight = product.getExpectedWeight();
+            long cost = product.getPrice();
+            
+            //update expected weight from bagging area
+                //use add item controller?
+                //not sure how to update weight otherwise
+                //handle weight discrepancy?
+
+            //Signal to Customer to place the scanned item in the bagging area
+
+            //Signals to System the weight has changed
+        }
+
+        //Unblock? 
+        AddItemController.unblock();
+    }
+}
