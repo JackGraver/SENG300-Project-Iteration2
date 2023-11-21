55dc0204b71a05bfb2da23dedf2ae5fad1ba7125
DingFei1 <123658306+DingFei1@users.noreply.github.com>
2023-11-20 21:07:23 -0700

    update the code as the hardware codes change

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/payWithBanknoteSilverControllerTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/payWithBanknoteSilverControllerTest.java
@@ -1,9 +1,16 @@
 package com.thelocalmarketplace.software.test;
 
+import com.jjjwelectronics.Numeral;
+import com.jjjwelectronics.scanner.Barcode;
 import com.tdc.CashOverloadException;
 import com.tdc.DisabledException;
 import com.tdc.banknote.*;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.Product;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationSilver;
+import com.thelocalmarketplace.hardware.external.ProductDatabases;
+import com.thelocalmarketplace.software.AddItem.StartSession;
+import com.thelocalmarketplace.software.Cart;
 import com.thelocalmarketplace.software.payWithBanknoteSilverController;
 
 import org.junit.Assert;
@@ -15,16 +22,16 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 /**
-	Jack Graver - 10187274
-	Christopher Thomson - 30186596
-	Shaim Momin - 30184418
-	Raja Muhammed Omar - 30159575
-	Michael Hoang - 30123605
-	Fei Ding - 30225995
-	Dylan Dizon - 30173525
-	Shenuk Perera - 30086618
-	Darpal Patel - 30088795
-	Md Abu Sinan - 30154627
+ Jack Graver - 10187274
+ Christopher Thomson - 30186596
+ Shaim Momin - 30184418
+ Raja Muhammed Omar - 30159575
+ Michael Hoang - 30123605
+ Fei Ding - 30225995
+ Dylan Dizon - 30173525
+ Shenuk Perera - 30086618
+ Darpal Patel - 30088795
+ Md Abu Sinan - 30154627
  */
 public class payWithBanknoteSilverControllerTest {
     payWithBanknoteSilverController testObject;
@@ -39,11 +46,16 @@ public class payWithBanknoteSilverControllerTest {
             new BigDecimal("0.1"),new BigDecimal("0.05")};
     int coinStorageUnitCapacityConfiguration = 5;
     Banknote[] testBanknote;
+    Barcode[] barcodes;
     final Banknote Banknote5 = new Banknote(Currency.getInstance("CAD"),new BigDecimal("5"));
     final Banknote Banknote10 = new Banknote(Currency.getInstance("CAD"),new BigDecimal("10"));
     final Banknote Banknote20 = new Banknote(Currency.getInstance("CAD"),new BigDecimal("20"));
     final Banknote Banknote50 = new Banknote(Currency.getInstance("CAD"),new BigDecimal("50"));
     final Banknote Banknote100 = new Banknote(Currency.getInstance("CAD"),new BigDecimal("100"));
+    BarcodedProduct product1;
+    BarcodedProduct product2;
+    BarcodedProduct product3;
+    Cart cart;
 
     @Before
     public void setUp() {
@@ -61,13 +73,37 @@ public class payWithBanknoteSilverControllerTest {
         station.plugIn(PowerGrid.instance());
         station.turnOn();
         testObject = new payWithBanknoteSilverController(station);
+        StartSession startSession = new StartSession(station);
+        Map<Barcode, BarcodedProduct> BARCODED_PRODUCT_DATABASE = ProductDatabases.BARCODED_PRODUCT_DATABASE;
+
+        Barcode barcode1 = new Barcode(new Numeral[]{Numeral.one, Numeral.two, Numeral.three, Numeral.four});
+        Barcode barcode2 = new Barcode(new Numeral[]{Numeral.five, Numeral.six, Numeral.seven, Numeral.eight});
+        Barcode barcode3 = new Barcode(new Numeral[]{Numeral.nine, Numeral.zero, Numeral.one, Numeral.two});
+
+        product1 = new BarcodedProduct(barcode1, "Product A", 5, 150.0);
+        product2 = new BarcodedProduct(barcode2, "Product B", 50, 200.0);
+        product3 = new BarcodedProduct(barcode3, "Product C", 11, 100.0);
+
+        BARCODED_PRODUCT_DATABASE.put(barcode1, product1);
+        BARCODED_PRODUCT_DATABASE.put(barcode2, product2);
+        BARCODED_PRODUCT_DATABASE.put(barcode3, product3);
+
+        barcodes = new Barcode[3];
+        barcodes[1]=barcode1;
+        barcodes[2]=barcode2;
+        barcodes[0]=barcode3;
+
+        // 创建Cart对象并传递其他对象作为参数
+        cart = new Cart(station, PowerGrid.instance(), startSession);
     }
 
     //add a correct set of banknotes
     @Test
     public void test1() throws DisabledException, CashOverloadException {
+        cart.addItem(product1, 1);
+        cart.addItem(product2, 1);
+        testObject.setCart(cart);
         testBanknote = new Banknote[]{Banknote5, Banknote50};
-        testObject.setTotalPrice(new BigDecimal("55"));
         testObject.payWithBanknote(Banknote5, Banknote50);
         Assert.assertEquals(2, testObject.banknoteInsertedNum);
         Assert.assertEquals(2, testObject.goodBanknoteNum);
@@ -78,7 +114,9 @@ public class payWithBanknoteSilverControllerTest {
     // add a bad banknote
     @Test
     public void test2() throws DisabledException, CashOverloadException {
-        testObject.setTotalPrice(new BigDecimal("55"));
+        cart.addItem(product1, 1);
+        cart.addItem(product2, 1);
+        testObject.setCart(cart);
         testObject.payWithBanknote(Banknote5, new Banknote(banknoteCurrencyConfiguration, new BigDecimal("59")));
         Assert.assertTrue(testObject.banknoteEjected);
         testObject.getStation().banknoteInput.removeDanglingBanknote();
@@ -92,7 +130,8 @@ public class payWithBanknoteSilverControllerTest {
     //make storage unit full
     @Test
     public void test3() throws DisabledException, CashOverloadException {
-        testObject.setTotalPrice(new BigDecimal("40"));
+        cart.addItem(product1, 8);
+        testObject.setCart(cart);
         testObject.payWithBanknote(Banknote5, Banknote5, Banknote5, Banknote5, Banknote5, Banknote5, Banknote5);
         //Assert.assertTrue(testObject.banknoteEjected);
         //testObject.getStation().banknoteInput.removeDanglingBanknote();
@@ -110,26 +149,11 @@ public class payWithBanknoteSilverControllerTest {
         Assert.assertTrue(testObject.banknotesFull);
     }
 
-    //need to make change
     @Test
     public void test4() throws DisabledException, CashOverloadException {
-        testObject.setTotalPrice(new BigDecimal("30"));
-        testObject.payWithBanknote(Banknote5, Banknote5, Banknote5, Banknote20);
-        Assert.assertEquals(4, testObject.banknoteInsertedNum);
-        Assert.assertEquals(4, testObject.goodBanknoteNum);
-        Assert.assertEquals(0, testObject.badBanknoteNum);
-        Assert.assertEquals(3, station.banknoteStorage.getBanknoteCount());
-        ArrayList<Banknote> expectedArrayList = new ArrayList<>(List.of(Banknote5));
-        ArrayList<Banknote> actuallArrayList = new ArrayList<>(station.banknoteOutput.danglingDispensedBanknotes);
-        for (int i = 0; i < Math.max(expectedArrayList.size(), actuallArrayList.size()); i++) {
-            Assert.assertEquals(expectedArrayList.get(i).getCurrency(), actuallArrayList.get(i).getCurrency());
-            Assert.assertEquals(expectedArrayList.get(i).getDenomination(), actuallArrayList.get(i).getDenomination());
-        }
-    }
-
-    @Test
-    public void test5() throws DisabledException, CashOverloadException {
-        testObject.setTotalPrice(new BigDecimal("55"));
+        cart.addItem(product1, 1);
+        cart.addItem(product2, 1);
+        testObject.setCart(cart);
         testObject.payWithBanknote(Banknote5);
         testObject.payWithBanknote(Banknote50);
         Assert.assertEquals(2, testObject.banknoteInsertedNum);
@@ -140,32 +164,34 @@ public class payWithBanknoteSilverControllerTest {
 
     //test a correct change
     @Test
-    public void test6() throws DisabledException, CashOverloadException {
+    public void test5() throws DisabledException, CashOverloadException {
+        cart.addItem(product1, 2);
+        cart.addItem(product2, 1);
+        testObject.setCart(cart);
         BigDecimal actualChangeAmount = new BigDecimal(BigInteger.ZERO);
         station.banknoteStorage.load(Banknote5, Banknote10, Banknote20, Banknote10, Banknote20);
-        testObject.setTotalPrice(new BigDecimal("60"));
         Assert.assertEquals(5, station.banknoteStorage.getBanknoteCount());
         testObject.payWithBanknote(Banknote100);
         Assert.assertEquals(1, testObject.banknoteInsertedNum);
         Assert.assertEquals(1, testObject.goodBanknoteNum);
         Assert.assertEquals(0, testObject.badBanknoteNum);
         Assert.assertEquals(3, station.banknoteStorage.getBanknoteCount());
-        Banknote[] actualChange = station.banknoteOutput.danglingDispensedBanknotes.toArray(new Banknote[0]);
+        Banknote[] actualChange = testObject.getChange();
         for (Banknote banknote: actualChange) {
             Assert.assertEquals(banknoteCurrencyConfiguration, banknote.getCurrency());
             actualChangeAmount = actualChangeAmount.add(banknote.getDenomination());
         }
         Assert.assertEquals(new BigDecimal("40"), actualChangeAmount);
-        station.banknoteOutput.removeDanglingBanknotes();
+        //station.banknoteOutput.removeDanglingBanknotes();
         Assert.assertTrue(testObject.banknotesRemoved);
-
     }
 
     //cannot change
     @Test
-    public void test7() throws DisabledException, CashOverloadException {
+    public void test6() throws DisabledException, CashOverloadException {
+        cart.addItem(product3, 1);
+        testObject.setCart(cart);
         station.banknoteStorage.load(Banknote5, Banknote10, Banknote20, Banknote10, Banknote20);
-        testObject.setTotalPrice(new BigDecimal("99"));
         Assert.assertEquals(5, station.banknoteStorage.getBanknoteCount());
         testObject.payWithBanknote(Banknote100);
         Assert.assertEquals(1, testObject.banknoteInsertedNum);
