acf5cdfbf73c8c56864931230979fb3f278995cf
Shenukp1 <shenuk.perera2002@gmail.com>
2023-11-20 23:28:27 -0700

    finished some stuff

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayCoinTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayCoinTest.java
@@ -25,15 +25,22 @@ import com.thelocalmarketplace.hardware.CoinTray;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationGold;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationSilver;
-import com.thelocalmarketplace.software.addItem.PaymentCoinController;
+import com.thelocalmarketplace.software.PaymentCoinController;
 
 import ca.ucalgary.seng300.simulation.SimulationException;
 import powerutility.PowerGrid;
-//Michael Svoboda (30039040)
-//Shenuk Perera (30086618)
-//Marvellous Chukwukelu (30197270)
-//Kyuyop Andrew Park(10046592)
-//Darpal Patel (30088795)
+/**
+Jack Graver - 10187274
+Christopher Thomson - 30186596
+Shaim Momin - 30184418
+Raja Muhammed Omar - 30159575
+Michael Hoang - 30123605
+Fei Ding - 30225995
+Dylan Dizon - 30173525
+Shenuk Perera - 30086618
+Darpal Patel - 30088795
+Md Abu Sinan - 30154627
+*/
 public class PayCoinTest {
 	
 	public SelfCheckoutStationGold selfCheckoutStationGold;
